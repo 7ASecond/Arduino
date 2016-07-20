54f4ea6c85a843cd43aed68936525e33970e10bc
Dave Gordon <admin@7asecond.net>
2016-07-20 19:15:54 +0100

    Added Hello World Initial 2

---
+++ Blink_Hello_World_2/Blink_Hello_World_2.ino
@@ -0,0 +1,133 @@
+#include <LiquidCrystal.h>
+
+/*
+  Blink
+  Turns on an LED on for one second, then off for one second, repeatedly.
+
+  Most Arduinos have an on-board LED you can control. On the Uno and
+  Leonardo, it is attached to digital pin 13. If you're unsure what
+  pin the on-board LED is connected to on your Arduino model, check
+  the documentation at http://www.arduino.cc
+
+  This example code is in the public domain.
+
+  modified 8 May 2014
+  by Scott Fitzgerald
+ */
+
+
+int DelayDot = 85;
+int DelayDash = DelayDot * 3;
+int DelayLetter = DelayDot * 2;
+int DelayWord = DelayDot * 7;
+
+LiquidCrystal Lcd(12,11,5,4,3,2);
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+  // initialize digital pin 13 as an output.
+  pinMode(13, OUTPUT);
+  Serial.begin(9600);
+  Lcd.begin(16,2);
+  Lcd.print("Hello");
+}
+
+// the loop function runs over and over again forever
+void loop() {
+Lcd.setCursor(0, 1);
+  // print the number of seconds since reset:
+  Lcd.print(millis() / 1000);
+
+  
+ // H
+ ShowDot(4);
+ EndOfLetter();
+
+ // E
+ShowDot(1);
+EndOfLetter();
+
+ // L
+ ShowDot(1);
+  ShowDash(1);
+  ShowDot(2);
+ EndOfLetter();
+
+ // L
+ShowDot(1);
+  ShowDash(1);
+  ShowDot(2);
+ EndOfLetter();
+ 
+ // O
+ ShowDash(3);
+ EndOfLetter();
+
+ EndOfWord();
+
+ // W
+ShowDash(1);
+ShowDot(2);
+ShowDash(1);
+EndOfLetter();
+
+ // O
+ ShowDash(3);
+ EndOfLetter();
+
+ // R
+ ShowDot(1);
+ ShowDash(1);
+ ShowDot(1);
+ EndOfLetter();
+
+ // L
+ ShowDot(1);
+ ShowDash(1);
+ ShowDot(2);
+ EndOfLetter();
+
+ // D
+ShowDash(1);
+ShowDot(2);
+EndOfWord(); 
+}
+
+void ShowDot(int x)
+{
+  for(int y = 0; y < x; y++)
+  {
+    tone(8, 500, DelayDot);
+    Serial.print(". ");
+    digitalWrite(13, HIGH);   // turn the LED on (HIGH is the voltage level)
+    delay(DelayDot);              // wait for a second
+    digitalWrite(13, LOW);    // turn the LED off by making the voltage LOW
+    delay(DelayDot);              // wait for a second
+  };
+}
+
+void ShowDash(int x)
+{
+  for(int y = 0; y < x; y++)
+  {
+    tone(8, 500, DelayDash);
+    Serial.print("- ");
+    digitalWrite(13, HIGH);   // turn the LED on (HIGH is the voltage level)
+    delay(DelayDash);              // wait for a second
+    digitalWrite(13, LOW);    // turn the LED off by making the voltage LOW
+    delay(DelayDot);              // wait for a second
+  };
+}
+
+void EndOfLetter()
+{  
+  Serial.print(" ");
+  delay(DelayLetter);
+}
+
+void EndOfWord()
+{
+ Serial.print("\n");
+ delay(DelayWord); 
+}
+
